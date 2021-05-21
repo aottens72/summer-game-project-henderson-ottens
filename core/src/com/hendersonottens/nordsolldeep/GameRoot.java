2b72e329edc1da93d85fe022314cf4d39906acab
August Ottens <ottens72@Augusts-MacBook-Pro.local>
2021-05-21 15:14:55 -0700

    changed back-end to recommended version

--- core/src/com/hendersonottens/nordsolldeep/GameRoot.java
+++ core/src/com/hendersonottens/nordsolldeep/GameRoot.java
@@ -12,14 +12,14 @@ public class GameRoot extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
--- desktop/src/com/hendersonottens/nordsolldeep/desktop/DesktopLauncher.java
+++ desktop/src/com/hendersonottens/nordsolldeep/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package com.hendersonottens.nordsolldeep.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.hendersonottens.nordsolldeep.GameRoot;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Nordsoll Deep";
-		config.width = 800;
-		config.height = 480;
-		new LwjglApplication(new GameRoot(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Nordsoll Deep");
+		config.setWindowedMode(800,480);
+		new Lwjgl3Application(new GameRoot(), config);
 	}
 }
