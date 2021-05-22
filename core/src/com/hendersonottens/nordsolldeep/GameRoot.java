4e564db44d1a441c0fac4d0a0f20051bb9908a8d
August Ottens <ottens72@Augusts-MacBook-Pro.local>
2021-05-21 17:47:54 -0700

    title screen created, non-function

--- core/src/com/hendersonottens/nordsolldeep/GameRoot.java
+++ core/src/com/hendersonottens/nordsolldeep/GameRoot.java
@@ -1,31 +1,48 @@
 package com.hendersonottens.nordsolldeep;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.hendersonottens.nordsolldeep.Screens.*;
 
-public class GameRoot extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class GameRoot extends Game {
+	private SpriteBatch batch;
+	private Texture[][] map;
+
+	static public Skin gameSkin;
+	private Stage stage;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		//batch = new SpriteBatch();
+		//stage = new Stage(new ScreenViewport());
 		//img = new Texture("badlogic.jpg");
+
+		gameSkin = new Skin(Gdx.files.internal("menu-font-skin/pixthulhu-ui.json"));
+		this.setScreen(new TitleScreen(this));
+		//render();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		//batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+//		ScreenUtils.clear(0, 0, 1, 1);
+//		stage.act();
+//		stage.draw();
+//
+//		batch.begin();
+//		//batch.draw(img, 0, 0);
+//		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
--- desktop/src/com/hendersonottens/nordsolldeep/desktop/DesktopLauncher.java
+++ desktop/src/com/hendersonottens/nordsolldeep/desktop/DesktopLauncher.java
@@ -1,12 +1,15 @@
 package com.hendersonottens.nordsolldeep.desktop;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.hendersonottens.nordsolldeep.GameRoot;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		DisplayMode primaryMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		//config.setFullscreenMode(primaryMode);
 		config.setTitle("Nordsoll Deep");
 		config.setWindowedMode(800,480);
 		new Lwjgl3Application(new GameRoot(), config);
