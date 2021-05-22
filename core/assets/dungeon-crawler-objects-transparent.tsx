a23db836fa86758393f8109e9c25d4dbaf0961d5
evanhenderson <ehenderson@zagmail.gonzaga.edu>
2021-05-22 18:18:03 -0400

    added first map (.tmx file) with asset files

---
+++ core/assets/dungeon-crawler-objects-transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="dungeon-crawler-objects-transparent" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="../top-down-collection-pack/top-down-collection-pack/Topview Fantasy Patreon Collection/single-dungeon-crawler-objects/PNG/dungeon-crawler-objects-transparent.png" width="272" height="144"/>
+</tileset>
---
+++ core/assets/dungeon-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="dungeon-tileset" tilewidth="32" tileheight="32" tilecount="36" columns="9">
+ <image source="../top-down-collection-pack/top-down-collection-pack/Topview Fantasy Patreon Collection/single-dungeon-crawler/PNG/dungeon-tileset.png" width="304" height="144"/>
+</tileset>
