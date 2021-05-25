b78b25ceadd43a86388d90d923fc7127c39a5c9f
August Ottens <ottensjacob@gmail.com>
2021-05-25 13:41:44 -0700

    rough collision detection working

---
+++ core/assets/maps/tile-assets/top-down-town-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="top-down-town-tileset" tilewidth="8" tileheight="8" tilecount="1248" columns="48" backgroundcolor="#00ffffff">
+ <image source="top-down-town-tileset.png" width="384" height="208"/>
+</tileset>
