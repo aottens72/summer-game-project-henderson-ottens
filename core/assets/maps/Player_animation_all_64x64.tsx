9d8ac596524f32a9f88cf0b80d75949b4bb48975
evanhenderson <ehenderson@zagmail.gonzaga.edu>
2021-06-15 22:30:46 -0400

    added player_animation files

---
+++ core/assets/maps/Player_animation_all_64x64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Player_animation_all_64x64 " tilewidth="32" tileheight="32" tilecount="108" columns="18">
+ <image source="D:/Player_animation_all_64x64 .png" width="576" height="192"/>
+</tileset>
---
+++ core/assets/maps/Player_animation_all_64x64_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Player_animation_all_64x64 " tilewidth="64" tileheight="64" tilecount="27" columns="9">
+ <image source="D:/Player_animation_all_64x64 .png" width="576" height="192"/>
+</tileset>
